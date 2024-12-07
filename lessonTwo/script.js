966345eba60fd23089070fb975e42d38f652ada1
Jammie506 <mccarthyjamie506@gmail.com>
2024-12-07 17:06:01 +0000

    Add Show/Hide Examples

--- lessonTwo/script.js
+++ lessonTwo/script.js
@@ -1,9 +1,21 @@
 $(document).ready(function() {
+    //The .click event would also work here, as well as .mouseEnter and .mouseLeave
     $("#stream1_btn").on("click", function() {
-        $(".stream1").removeClass('highlight_stream');
-        $(".stream2").removeClass('highlight_stream');
-        $(".stream3").removeClass('highlight_stream');
-        $(".stream1").addClass('highlight_stream');
+        $(".stream1").toggle();
+        /*
+        $(".stream1").show();
+        $(".stream1").show('slow');
+        $(".stream1").show('medium');
+        $(".stream1").show('fast');
+        $(".stream1").show('1000');
+        */
+        /*
+        $(".stream1").hide();
+        $(".stream1").hide('slow');
+        $(".stream1").hide('medium');
+        $(".stream1").hide('fast');
+        $(".stream1").hide('1000');
+        */
    });
    $("#stream2_btn").on("click", function() {
         $(".stream1").removeClass('highlight_stream');
